0fb5b67cdc5ca9cfe8c01942c4f1909532a6beb7
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-03 21:22:06 +0100

    New component BaseSearch

---
+++ components/base/Search.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="relative">
+        <input 
+            class="bg-white rounded-full py-1 px-3 pe-9 w-full"
+            type="text" 
+            :placeholder="placeholder"
+            :value="modelValue"
+            @input="(event) => $emit('update:modelValue', event.target.value)"
+        />
+
+        <span class="absolute inset-y-0 end-0 grid w-10 place-content-center">
+            <Icon :name="icon" size="20" />
+        </span>
+    </div>
+
+</template>
+
+
+<script setup>
+// Composent de recherche typescript: placeholder, icon, model-value etc ...
+const props = defineProps({
+    placeholder: {
+        type: String,
+        default: 'Search'
+    },
+    modelValue: {
+        type: String,
+        default: ''
+    },
+    icon: {
+        type: String,
+        default: 'eva:search-outline'
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
