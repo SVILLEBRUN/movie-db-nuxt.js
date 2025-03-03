53b16e8447cf7599b539e662e561c63ee303a90b
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-03 21:38:47 +0100

    New component NavBar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,57 +1,7 @@
 <template>
     <div class="min-h-screen flex flex-col">
         <!-- Navebar / Header -->
-        <div class="bg-gray-800 sticky top-0 z-50">
-            <div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-                <NuxtLink class="block text-white font-bold text-4xl" to="/">
-                    MES FILMS
-                </NuxtLink>
-
-                <div class="flex flex-1 items-center justify-end md:justify-between gap-8">
-                    <!-- Search bar large media -->
-                    <div class="w-full hidden sm:block">
-                        <BaseSearch v-model="query" @keyup.enter="search()"/>
-                    </div>
-                    <div class="flex items-center gap-4">
-                        <div class="sm:flex sm:gap-4">
-                            <NuxtLink
-                                class="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-500"
-                                to="/login"
-                            >
-                                Login
-                            </NuxtLink>
-
-                            <NuxtLink
-                                class="hidden rounded-md px-5 py-2.5 text-sm font-medium transition sm:block dark:bg-gray-900 text-white hover:text-white/75"
-                                href="/register"
-                            >
-                                Register
-                            </NuxtLink>
-                        </div>
-
-                        <!-- Bouton de menu au cas ou j'en ai besoins -->
-                        <!-- <button
-                            class="block rounded-sm p-2.5 transition md:hidden text-white hover:text-white/75"
-                        >
-                            <span class="sr-only">Toggle menu</span>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="size-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                stroke-width="2"
-                            >
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                        </button> -->
-                    </div>
-                </div>
-            </div>
-            <div class="w-full sm:hidden px-2 mb-2">
-                <BaseSearch v-model="query" @keyup.enter="search()"/>
-            </div>
-        </div>
+        <NavBar />
 
         <!-- Main -->
         <div class="bg-slate-900 relative text-white flex-grow">
@@ -66,22 +16,3 @@
         </div>
     </div>
 </template>
-
-<script setup >
-const query = ref('');
-
-const router = useRouter()
-
-const search = () => {
-    // Function to search data (person / movie) 
-    console.log(query.value)
-
-    router.push({
-        path: '/search',
-        query: {
-            query: query.value
-        }
-    })
-}
-</script>
-
