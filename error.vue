fac587ea8d6885bb287e9320f0bf8e7be2e1b117
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-03 16:30:35 +0100

    setup error page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-slate-900 text-white h-screen flex flex-col justify-center items-center">
+        <h1 class="tracking-widest">{{ error.statusCode }} | {{ error.message }}</h1>
+        <NuxtLink to="/" class="mt-7 py-1 px-3 bg-slate-800 rounded hover:bg-slate-700">Go back home</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+</script>
+
+<style scoped>
+
+</style>
