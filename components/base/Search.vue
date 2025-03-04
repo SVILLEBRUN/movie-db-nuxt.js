152940cdbb354855805ef2164e0084fa109cf17f
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-04 14:04:22 +0100

    Revert "Changement du composent search en Input"
    This reverts commit a52a3dcfb702fbfe4915e88c1322ed35bec2ff32.

--- components/base/Search.vue
+++ components/base/Search.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="relative">
         <input 
-            class="bg-white rounded-full py-1 px-3 w-full"
-            :class="icon ? 'pe-9' : ''"
-            :type="type" 
+            class="bg-white rounded-full py-1 px-3 pe-9 w-full"
+            type="text" 
             :placeholder="placeholder"
             :value="modelValue"
             @input="(event) => $emit('update:modelValue', event.target.value)"
         />
 
-        <span v-if="icon" class="absolute inset-y-0 end-0 grid w-10 place-content-center">
+        <span class="absolute inset-y-0 end-0 grid w-10 place-content-center">
             <Icon :name="icon" size="20" />
         </span>
     </div>
@@ -22,7 +21,7 @@
 const props = defineProps({
     placeholder: {
         type: String,
-        default: ''
+        default: 'Search'
     },
     modelValue: {
         type: String,
@@ -30,11 +29,7 @@ const props = defineProps({
     },
     icon: {
         type: String,
-        default: ''
-    },
-    type: {
-        type: String,
-        default: 'text'
+        default: 'eva:search-outline'
     }
 })
 
