0e97b9ee581905ad4244f1ab790ecada1dee60bf
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-14 23:51:55 +0100

    renaming variables

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -6,16 +6,16 @@ export default defineNuxtRouteMiddleware((to, from) => {
         checkAuth();
     }
 
-    const publicRoutes = ['/login', '/register'];
-    const isPublicRoute = publicRoutes.includes(to.path);
+    const authRoutes = ['/login', '/register', '/password_reset'];
+    const isAhthRoute = authRoutes.includes(to.path);
 
     // Si l'utilisateur est connecté et essaie d'aller sur login/register, on le redirige à l'accueil
-    if (authStore.isLoggedIn && isPublicRoute) {
+    if (authStore.isLoggedIn && isAhthRoute) {
         return navigateTo('/');
     }
 
     // Si l'utilisateur n'est pas connecté et essaie d'aller ailleurs que login/register, on le redirige vers /login
-    if (!authStore.isLoggedIn && !isPublicRoute) {
+    if (!authStore.isLoggedIn && !isAhthRoute) {
         return navigateTo('/login');
     }
 
