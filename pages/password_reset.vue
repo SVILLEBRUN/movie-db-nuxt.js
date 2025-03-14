cdb7631e7278cf32b1f2003a6d7bdf39dea446a7
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-15 00:16:33 +0100

    new password_reset page

---
+++ pages/password_reset.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-sm sm:text-base">
+        <div>Password Reset</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'login',
+    middleware: 'auth'
+})
+</script>
