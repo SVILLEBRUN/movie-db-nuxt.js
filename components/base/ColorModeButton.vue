d5daa23937e0783763d91094a204de311c150899
Sébastien VILLEBRUN <104035897+SVILLEBRUN@users.noreply.github.com>
2025-03-14 17:37:41 +0100

    New dark-mode switch to the NavBar

---
+++ components/base/ColorModeButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <ClientOnly v-if="!colorMode?.forced">
+        <UButton
+            :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+            color="neutral"
+            variant="ghost"
+            @click="isDark = !isDark"
+        />
+            <template #fallback>
+                <div class="size-8" />
+            </template>
+    </ClientOnly>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+console.log(colorMode)
+
+const isDark = computed({
+    get() {
+        return colorMode.value === 'dark'
+    },
+    set() {
+        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+})
+</script>
